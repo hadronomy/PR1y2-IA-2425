9288f852f8b4922ebc40e6ab7ce80ba71a22b64f
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-10-06 16:03:55 +0100

    chore: remove `ia.parser` module

--- ia/parser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Consists of the different parsers used in the project.
-
-The parsers are used to read the files and convert
-them into a format that can be used by the project.
-"""
-
-from .graph import undirected as undirected
