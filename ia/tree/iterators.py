ef6f523eb17297779500b3b0235cece1744a0efb
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-10-06 23:23:57 +0100

    feat: add `postorder` iterator

--- ia/tree/iterators.py
+++ ia/tree/iterators.py
@@ -27,3 +27,24 @@ def preorder_iter(
         yield tree
         for child in tree.children:
             yield from preorder_iter(child, max_depth)
+
+
+def postorder_iter(
+    tree: T,
+    max_depth: int = 0,
+) -> Iterable[T]:
+    """Inorder iterator for tree.
+
+    Parameters
+    ----------
+        tree : (NodeT)
+            Tree to iterate.
+
+    Returns
+    -------
+        (Iterable[NodeT])
+    """
+    if tree and (not max_depth or not tree.get_attribute("depth") > max_depth):
+        for child in tree.children:
+            yield from postorder_iter(child, max_depth)
+        yield tree
