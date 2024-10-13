25c10ef858b5328f04bb34c504f96a1dba9ca371
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-10-13 17:00:11 +0100

    feat: change the wall character for detailed print

--- ia/maze/constants.py
+++ ia/maze/constants.py
@@ -6,7 +6,7 @@ ALPHABET = [chr(letter) for letter in range(65, 91)]
 NUMBERS = [str(number) for number in range(0, 10)]
 
 MAZE_PRINT_STYLES = {
-    MazeTile.WALL: "█",
+    MazeTile.WALL: "■",
     MazeTile.EMPTY: " ",
     MazeTile.START: "S",
     MazeTile.GOAL: "G",
